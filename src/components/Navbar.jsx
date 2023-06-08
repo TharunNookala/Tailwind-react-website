6b403d511879c51670a62cade6edba1e50b65f26
TharunNookala <iamtharun7799@gmail.com>
2023-06-09 00:19:57 +0530

    navbar fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
                 <li className='p-4 cursor-pointer'>Contact</li>
             </ul>
             <div onClick={handleNav} className='block md:hidden'>
-                {!nav ? <AiOutlineClose size={23} /> : <AiOutlineMenu size={23} />}
+                {!nav ? <AiOutlineMenu size={23} /> : <AiOutlineClose size={23} />}
             </div>
             <div className={nav ? 'fixed left-0 top-0 w-[40%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
                 <h1 className='w-full text-3xl font-bold text-[#00df9a] mt-8 mb-4 px-4'>&lt;tharun/&gt;</h1>
