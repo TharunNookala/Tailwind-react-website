0508168afd29f065bc7cee4c08be6e179ae2d351
TharunNookala <iamtharun7799@gmail.com>
2023-06-08 18:34:56 +0530

    navbar fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
             <div onClick={handleNav} className='block md:hidden'>
                 {!nav ? <AiOutlineClose size={23} /> : <AiOutlineMenu size={23} />}
             </div>
-            <div className={!nav ? 'fixed left-0 top-0 w-[40%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'fixed left-[-100%] hidden'}>
+            <div className={nav ? 'fixed left-0 top-0 w-[40%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
                 <h1 className='w-full text-3xl font-bold text-[#00df9a] mt-8 mb-4 px-4'>&lt;tharun/&gt;</h1>
                 <ul className='uppercase p-4'>
                     <li className='p-4 border-b border-gray-500 cursor-pointer'>Home</li>
